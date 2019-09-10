77ca232aa428bfcd2aa8de677baeecbac22ff73c
Murtadho108 <33507864+Murtadho108@users.noreply.github.com>
2019-09-10 12:50:53 +0700

    Update 9_self_join.sql

--- 9_self_join.sql
+++ 9_self_join.sql
@@ -40,4 +40,23 @@ FROM route a JOIN route b ON
 WHERE stopa.name='Craiglockhart' and stopb.name = 'London Road';
 
 -- 7
--- 
+-- Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+
+SELECT DISTINCT a.company, a.num FROM route a JOIN route b
+ON a.company = b.company AND a.num = b.num
+WHERE a.stop = 115 AND b.stop = 137;
+
+-- 8
+-- Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+
+SELECT a.company, a.num FROM route a JOIN route b 
+ON a.company = b.company AND a.num = b.num
+JOIN stops stopa ON a.stop = stopa.id
+JOIN stops stopb ON b.stop = stopb.id
+WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross';
+
+-- 9
+-- Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. 
+-- Include the company and bus no. of the relevant services.
+
+
