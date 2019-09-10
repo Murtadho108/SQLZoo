b8f8be01e0d06c85f9191750c21237f2ea0ba24e
Murtadho108 <33507864+Murtadho108@users.noreply.github.com>
2019-09-10 12:29:36 +0700

    Update 8_using_null.sql

--- 8_using_null.sql
+++ 8_using_null.sql
@@ -51,7 +51,6 @@ ELSE 'Art' end  FROM teacher;
 -- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, 
 -- show 'Art' if the teacher's dept is 3 and 'None' otherwise.
 
-SELECT name, CASE WHEN dept = 1 OR dept = 2 THEN 'Sci'
-ELSE 'Art' END  FROM teacher;
+SELECT name, CASE WHEN dept = 1 OR dept = 2 THEN 'Sci' ELSE 'Art' END  FROM teacher;
 
 
